82415e38e896211401b49664e4bd47e78dd7ab7a
Berro-1 <hussienberro1@gmail.com>
2024-07-27 16:51:40 +0300

    Added main layout to app

--- code-editor-front/src/App.js
+++ code-editor-front/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React, { Component } from "react";
+import { Box, Container } from "@mui/material";
+import Login from "./pages/login/Loginn";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Box className="app-container">
+        <Container maxWidth="xs">
+          <Login />
+        </Container>
+      </Box>
+    );
+  }
 }
 
 export default App;
