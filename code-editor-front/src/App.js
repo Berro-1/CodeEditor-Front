6dc943c0ddc308c6447871db712f995e3b7c00ef
Berro-1 <hussienberro1@gmail.com>
2024-07-29 17:23:47 +0300

    added routes and packages

--- code-editor-front/src/App.js
+++ code-editor-front/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Login from "./pages/login/Loginn";
+import Users from "./pages/adminDashboard/Users";
+import ChatList from "./pages/chatList/ChatList";
+import Chat from "./pages/chat/Chat";
 import "./App.css";
 import {
   createBrowserRouter,
@@ -9,13 +12,14 @@ import {
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Users from "./pages/adminDashboard/Users";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<Users />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/chats" element={<ChatList />} />
+        <Route path="/chats/:chatId" element={<Chat />} />
       </>
     )
   );
