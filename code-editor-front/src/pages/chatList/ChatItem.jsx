8e9f790aeb1ab85972ee2f88251a9bbe0c7c7ea2
Berro-1 <hussienberro1@gmail.com>
2024-07-30 04:09:47 +0300

    Adjusted Chat Item component to retrieve username

--- code-editor-front/src/pages/chatList/ChatItem.jsx
+++ code-editor-front/src/pages/chatList/ChatItem.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './ChatList.css';
 
 const ChatItem = ({ chat, userId, openChat }) => {
-  const chatPartner = chat.user2 === userId ? chat.user1 : chat.user2;
+  const chatPartnerName = chat.user2 === userId ? chat.user1Name : chat.user2Name;
 
   return (
     <li className="chatItem" onClick={() => openChat(chat.id)}>
       <div className="chatInfo">
-        <span>Chat with User {chatPartner}</span>
+        <span>Chat with {chatPartnerName}</span>
         <span className="chatPreview">{chat.latestMessage || 'No messages yet'}</span>
       </div>
     </li>
