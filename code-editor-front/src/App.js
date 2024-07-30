b34e939ec1f97ec8054f1a4af171b2c5ac9b7355
Berro-1 <hussienberro1@gmail.com>
2024-07-30 16:37:12 +0300

    Added layout for app

--- code-editor-front/src/App.js
+++ code-editor-front/src/App.js
@@ -1,26 +1,42 @@
 import React from "react";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import NavBar from "./components/navBar/navBar";
 import Login from "./pages/login/Loginn";
 import Signup from "./pages/signup/signup";
 
+
 import "./App.css";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
+const LayoutWithNavBar = () => (
+  <div>
+    <NavBar />
+    <Outlet />
+  </div>
+);
+
+const LayoutWithoutNavBar = () => (
+  <div>
+    <Outlet />
+  </div>
+);
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route element={<LayoutWithNavBar />}>
+        </Route>
+        <Route element={<LayoutWithoutNavBar />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
       </>
     )
   );
+
   return (
     <div>
       <ToastContainer />
