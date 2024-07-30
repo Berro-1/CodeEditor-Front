265ed3695872f67c3b2ce7f13bf9a152fb2df11c
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-30 23:38:20 +0300

    Fixed api

--- code-editor-front/src/pages/login/Loginn.jsx
+++ code-editor-front/src/pages/login/Loginn.jsx
@@ -1,40 +1,42 @@
-import React, { useState } from 'react';
-import { TextField, Button, Typography, Paper } from '@mui/material';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './Login.css';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@mui/material";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./Login.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
   const login = async (email, password) => {
     try {
       if (!email || !password) {
-        toast.error('Please enter both email and password');
+        toast.error("Please enter both email and password");
         return;
       }
-      const response = await axios.post('http://127.0.0.1:8000/api/auth/login', {
+      const response = await axios.post("http://127.0.0.1:8000/api/login", {
         email: email,
-        password: password
+        password: password,
       });
-      toast.success('Login successful');
+      toast.success("Login successful");
       console.log(response);
-      navigate('/home')
+      navigate("/home");
     } catch (error) {
-      toast.error('Error logging in');
-      console.log('Error logging in', error);
+      toast.error("Error logging in");
+      console.log("Error logging in", error);
     }
-  }
+  };
 
   return (
-    <Paper
-      elevation={6}
-      className="login-paper"
-    >
-      <Typography variant="h4" gutterBottom align="center" className="login-title">
+    <Paper elevation={6} className="login-paper">
+      <Typography
+        variant="h4"
+        gutterBottom
+        align="center"
+        className="login-title"
+      >
         Login
       </Typography>
       <div>
@@ -46,10 +48,10 @@ const navigate = useNavigate();
           onChange={(e) => setEmail(e.target.value)}
           margin="normal"
           InputLabelProps={{
-            className: 'input-label',
+            className: "input-label",
           }}
           InputProps={{
-            className: 'input-text',
+            className: "input-text",
           }}
         />
         <TextField
@@ -61,10 +63,10 @@ const navigate = useNavigate();
           onChange={(e) => setPassword(e.target.value)}
           margin="normal"
           InputLabelProps={{
-            className: 'input-label',
+            className: "input-label",
           }}
           InputProps={{
-            className: 'input-text',
+            className: "input-text",
           }}
         />
         <Button
